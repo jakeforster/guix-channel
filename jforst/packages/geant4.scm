9c27d2cfffea5772f066d31a2a4c99a28936fd3f
Jake Forster <jakecameron.forster@gmail.com>
2023-11-30 19:17:57 +1030

    Add DAWN.

--- jforst/packages/geant4.scm
+++ jforst/packages/geant4.scm
@@ -110,13 +110,13 @@
   (package
     (name "clhep")
     (version "2.3.4.3")
-    (source (origin
-              (method url-fetch)
-              (uri
-               "https://gitlab.cern.ch/CLHEP/CLHEP/-/archive/CLHEP_2_3_4_3/CLHEP-CLHEP_2_3_4_3.tar.gz")
-              (sha256
-               (base32
-                "0h1mpc795lzq0z562jiq8w7rv0l1kwjjb98ynydqmxb0djnsmsli"))))
+    (source
+     (origin
+       (method url-fetch)
+       (uri
+        "https://gitlab.cern.ch/CLHEP/CLHEP/-/archive/CLHEP_2_3_4_3/CLHEP-CLHEP_2_3_4_3.tar.gz")
+       (sha256
+        (base32 "0h1mpc795lzq0z562jiq8w7rv0l1kwjjb98ynydqmxb0djnsmsli"))))
     (build-system cmake-build-system)
     (home-page "https://proj-clhep.web.cern.ch/proj-clhep/")
     (synopsis "HEP-specific foundation and utility classes")
@@ -130,13 +130,13 @@ CLHEP is structured in a set of packages independent of any external package.")
   (package
     (inherit clhep-2.3.4.3)
     (version "2.4.6.2")
-    (source (origin
-              (method url-fetch)
-              (uri
-               "https://gitlab.cern.ch/CLHEP/CLHEP/-/archive/CLHEP_2_4_6_2/CLHEP-CLHEP_2_4_6_2.tar.gz")
-              (sha256
-               (base32
-                "18sm14ikdz8hym5b2c9yb5l4hvzjk77jfasahb8zs41fcx6r9gwp"))))))
+    (source
+     (origin
+       (method url-fetch)
+       (uri
+        "https://gitlab.cern.ch/CLHEP/CLHEP/-/archive/CLHEP_2_4_6_2/CLHEP-CLHEP_2_4_6_2.tar.gz")
+       (sha256
+        (base32 "18sm14ikdz8hym5b2c9yb5l4hvzjk77jfasahb8zs41fcx6r9gwp"))))))
 
 ;; -- Geant4 datasets--
 
@@ -148,8 +148,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (method url-fetch)
       (uri (string-append "https://cern.ch/geant4-data/datasets/G4NDL."
                           version ".tar.gz"))
-      (sha256 (base32
-               "022l2jjhi57frfdv9nk6s6q23gmr9zkix06fmni8gf0gmvr7qa4x")))))
+      (sha256 (base32 "022l2jjhi57frfdv9nk6s6q23gmr9zkix06fmni8gf0gmvr7qa4x")))))
 
 (define g4ndl-4.7
   (let ((version "4.7"))
@@ -157,8 +156,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (method url-fetch)
       (uri (string-append "https://cern.ch/geant4-data/datasets/G4NDL."
                           version ".tar.gz"))
-      (sha256 (base32
-               "0283cwylajyjm4267ngfc2bd3452623r5bakywaccb8h44k3szby")))))
+      (sha256 (base32 "0283cwylajyjm4267ngfc2bd3452623r5bakywaccb8h44k3szby")))))
 
 ;; G4EMLOW
 ;;
@@ -168,8 +166,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (method url-fetch)
       (uri (string-append "https://cern.ch/geant4-data/datasets/G4EMLOW."
                           version ".tar.gz"))
-      (sha256 (base32
-               "1jrw0izw732bywq1k1srs3x2z0m3y2h377kcvwbwcr0wa1p10342")))))
+      (sha256 (base32 "1jrw0izw732bywq1k1srs3x2z0m3y2h377kcvwbwcr0wa1p10342")))))
 
 (define g4emlow-8.2
   (let ((version "8.2"))
@@ -177,8 +174,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (method url-fetch)
       (uri (string-append "https://cern.ch/geant4-data/datasets/G4EMLOW."
                           version ".tar.gz"))
-      (sha256 (base32
-               "09z4m3hq6895s7vwiaham7zbfq0ww6xh8xh8jv5kp9gm9wk6hxrx")))))
+      (sha256 (base32 "09z4m3hq6895s7vwiaham7zbfq0ww6xh8xh8jv5kp9gm9wk6hxrx")))))
 
 ;; PhotonEvaporation
 ;;
@@ -189,8 +185,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (uri (string-append
             "https://cern.ch/geant4-data/datasets/G4PhotonEvaporation."
             version ".tar.gz"))
-      (sha256 (base32
-               "1mvnbs7yvkii41blks6bkqr8qhxgnj3xxvv1i3vdg2y14shxv5ar")))))
+      (sha256 (base32 "1mvnbs7yvkii41blks6bkqr8qhxgnj3xxvv1i3vdg2y14shxv5ar")))))
 
 (define photon-evaporation-5.7
   (let ((version "5.7"))
@@ -199,8 +194,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (uri (string-append
             "https://cern.ch/geant4-data/datasets/G4PhotonEvaporation."
             version ".tar.gz"))
-      (sha256 (base32
-               "1rg7fygfxx06h98ywlci6b0b9ih74q8diygr76c3vppxdzjl47kn")))))
+      (sha256 (base32 "1rg7fygfxx06h98ywlci6b0b9ih74q8diygr76c3vppxdzjl47kn")))))
 
 ;; RadioactiveDecay
 ;;
@@ -211,8 +205,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (uri (string-append
             "https://cern.ch/geant4-data/datasets/G4RadioactiveDecay." version
             ".tar.gz"))
-      (sha256 (base32
-               "0qaark6mqzxr3lqawv6ai8z5211qihlp5x2hn86vzx8kgpd7j1r4")))))
+      (sha256 (base32 "0qaark6mqzxr3lqawv6ai8z5211qihlp5x2hn86vzx8kgpd7j1r4")))))
 
 (define radioactive-decay-5.6
   (let ((version "5.6"))
@@ -221,8 +214,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (uri (string-append
             "https://cern.ch/geant4-data/datasets/G4RadioactiveDecay." version
             ".tar.gz"))
-      (sha256 (base32
-               "1w8d9zzc4ss7sh1f8cxv5pmrx2b74p1y26377rw9hnlfkiy0g1iq")))))
+      (sha256 (base32 "1w8d9zzc4ss7sh1f8cxv5pmrx2b74p1y26377rw9hnlfkiy0g1iq")))))
 
 ;; G4PARTICLEXS
 ;;
@@ -232,8 +224,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (method url-fetch)
       (uri (string-append "https://cern.ch/geant4-data/datasets/G4PARTICLEXS."
             version ".tar.gz"))
-      (sha256 (base32
-               "0h8ba8jk197npbd9lzq2qlfiklbjgqwk45m1cc6piy5vf8ri0k89")))))
+      (sha256 (base32 "0h8ba8jk197npbd9lzq2qlfiklbjgqwk45m1cc6piy5vf8ri0k89")))))
 
 (define g4particlexs-4.0
   (let ((version "4.0"))
@@ -241,8 +232,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (method url-fetch)
       (uri (string-append "https://cern.ch/geant4-data/datasets/G4PARTICLEXS."
             version ".tar.gz"))
-      (sha256 (base32
-               "15fa6c8jh6g3nj82ychc13wlz2rc58v9jjdb6vyv1wn30fbh70ck")))))
+      (sha256 (base32 "15fa6c8jh6g3nj82ychc13wlz2rc58v9jjdb6vyv1wn30fbh70ck")))))
 
 ;; G4PII
 ;;
@@ -252,8 +242,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (method url-fetch)
       (uri (string-append "https://cern.ch/geant4-data/datasets/G4PII."
                           version ".tar.gz"))
-      (sha256 (base32
-               "09p92rk1sj837m6n6yd9k9a8gkh6bby2bfn6k0f3ix3m4s8as9b2")))))
+      (sha256 (base32 "09p92rk1sj837m6n6yd9k9a8gkh6bby2bfn6k0f3ix3m4s8as9b2")))))
 
 ;; RealSurface
 ;;
@@ -264,8 +253,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (uri (string-append
             "https://cern.ch/geant4-data/datasets/G4RealSurface." version
             ".tar.gz"))
-      (sha256 (base32
-               "0l3gs0nlp10cjlwiln3f72zfch0av2g1r8m2ny9afgvwgbwiyj4h")))))
+      (sha256 (base32 "0l3gs0nlp10cjlwiln3f72zfch0av2g1r8m2ny9afgvwgbwiyj4h")))))
 
 (define real-surface-2.2
   (let ((version "2.2"))
@@ -274,8 +262,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (uri (string-append
             "https://cern.ch/geant4-data/datasets/G4RealSurface." version
             ".tar.gz"))
-      (sha256 (base32
-               "08382y1258ifs7nap6zaaazvabg72blr0dkqgwk32lrg07hdwm4r")))))
+      (sha256 (base32 "08382y1258ifs7nap6zaaazvabg72blr0dkqgwk32lrg07hdwm4r")))))
 
 ;; G4SAIDDATA
 ;;
@@ -285,8 +272,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (method url-fetch)
       (uri (string-append "https://cern.ch/geant4-data/datasets/G4SAIDDATA."
                           version ".tar.gz"))
-      (sha256 (base32
-               "149fqy801n1pj2g6lcai2ziyvdz8cxdgbfarax6y8wdakgksh9hx")))))
+      (sha256 (base32 "149fqy801n1pj2g6lcai2ziyvdz8cxdgbfarax6y8wdakgksh9hx")))))
 
 ;; G4ABLA
 ;;
@@ -296,8 +282,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (method url-fetch)
       (uri (string-append "https://cern.ch/geant4-data/datasets/G4ABLA."
                           version ".tar.gz"))
-      (sha256 (base32
-               "1v97q28g1xqwnav0lwzwk7hc3b87yrmbvkgadf4bkwcbnm9b163n")))))
+      (sha256 (base32 "1v97q28g1xqwnav0lwzwk7hc3b87yrmbvkgadf4bkwcbnm9b163n")))))
 
 ;; G4INCL
 ;;
@@ -307,8 +292,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (method url-fetch)
       (uri (string-append "https://cern.ch/geant4-data/datasets/G4INCL."
                           version ".tar.gz"))
-      (sha256 (base32
-               "0z9nqk125vvf4f19lhgb37jy60jf9zrjqg5zbxbd1wz93a162qbi")))))
+      (sha256 (base32 "0z9nqk125vvf4f19lhgb37jy60jf9zrjqg5zbxbd1wz93a162qbi")))))
 
 ;; G4ENSDFSTATE
 ;;
@@ -318,8 +302,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (method url-fetch)
       (uri (string-append "https://cern.ch/geant4-data/datasets/G4ENSDFSTATE."
             version ".tar.gz"))
-      (sha256 (base32
-               "19p0sq0rmyg48j9hddqy24dn99md7ddiyq09lyj381q7cbpjfznx")))))
+      (sha256 (base32 "19p0sq0rmyg48j9hddqy24dn99md7ddiyq09lyj381q7cbpjfznx")))))
 
 (define g4ensdfstate-2.3
   (let ((version "2.3"))
@@ -327,8 +310,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (method url-fetch)
       (uri (string-append "https://cern.ch/geant4-data/datasets/G4ENSDFSTATE."
             version ".tar.gz"))
-      (sha256 (base32
-               "00wjir59rrrlk0a12vi8rsnhyya71rdi1kmark9sp487hbhcai4l")))))
+      (sha256 (base32 "00wjir59rrrlk0a12vi8rsnhyya71rdi1kmark9sp487hbhcai4l")))))
 
 ;; G4TENDL
 ;;
@@ -338,8 +320,7 @@ CLHEP is structured in a set of packages independent of any external package.")
       (method url-fetch)
       (uri (string-append "https://cern.ch/geant4-data/datasets/G4TENDL."
                           version ".tar.gz"))
-      (sha256 (base32
-               "1q11jxfy5kjwb0jrvwv6dgdxr3h85s6g2bl9kdbfvd681h178wjb")))))
+      (sha256 (base32 "1q11jxfy5kjwb0jrvwv6dgdxr3h85s6g2bl9kdbfvd681h178wjb")))))
 
 ;; -- Geant4 without visualisation -- 
 
@@ -349,14 +330,13 @@ CLHEP is structured in a set of packages independent of any external package.")
   (package
     (name "geant4")
     (version "11.1.1")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append
-                    "http://cern.ch/geant4-data/releases/geant4-v" version
-                    ".tar.gz"))
-              (sha256
-               (base32
-                "0sg45nd47ajqhzncw5fahrrx8a9s54x1l31l6xa73xsx5bmj54fj"))))
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "http://cern.ch/geant4-data/releases/geant4-v"
+                           version ".tar.gz"))
+       (sha256
+        (base32 "0sg45nd47ajqhzncw5fahrrx8a9s54x1l31l6xa73xsx5bmj54fj"))))
     (build-system cmake-build-system)
     (inputs (list coreutils
                   gcc-toolchain
@@ -464,28 +444,25 @@ can use @code{geant4-vis} for that.")
   (package
     (inherit geant4-11-1-1)
     (version "11.1.2")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append
-                    "http://cern.ch/geant4-data/releases/geant4-v" version
-                    ".tar.gz"))
-              (sha256
-               (base32
-                "1ma7qswqf320v03ry3j9sc20rjvn5ailp3g9k32bk7vlw29jsy3v"))))))
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "http://cern.ch/geant4-data/releases/geant4-v"
+                           version ".tar.gz"))
+       (sha256
+        (base32 "1ma7qswqf320v03ry3j9sc20rjvn5ailp3g9k32bk7vlw29jsy3v"))))))
 
 (define-public geant4-11-1-3
   (package
     (inherit geant4-11-1-1)
     (version "11.1.3")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append
-                    "http://cern.ch/geant4-data/releases/geant4-v" version
-                    ".tar.gz"))
-              (sha256
-               (base32
-                "1xcj4vigk7mb7wylr5zr3c7ac4ajdcfn786cvag633cxg2kbwpsc"))))))
-
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "http://cern.ch/geant4-data/releases/geant4-v"
+                           version ".tar.gz"))
+       (sha256
+        (base32 "1xcj4vigk7mb7wylr5zr3c7ac4ajdcfn786cvag633cxg2kbwpsc"))))))
 
 ;; -- Geant4 with visualisation using OGL + Qt -- 
 
@@ -595,24 +572,63 @@ This package supports visualisation with OpenGL and Qt.")))
   (package
     (inherit geant4-vis-11-1-1)
     (version "11.1.2")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append
-                    "http://cern.ch/geant4-data/releases/geant4-v" version
-                    ".tar.gz"))
-              (sha256
-               (base32
-                "1ma7qswqf320v03ry3j9sc20rjvn5ailp3g9k32bk7vlw29jsy3v"))))))
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "http://cern.ch/geant4-data/releases/geant4-v"
+                           version ".tar.gz"))
+       (sha256
+        (base32 "1ma7qswqf320v03ry3j9sc20rjvn5ailp3g9k32bk7vlw29jsy3v"))))))
 
 (define-public geant4-vis-11-1-3
   (package
     (inherit geant4-vis-11-1-1)
     (version "11.1.3")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append
-                    "http://cern.ch/geant4-data/releases/geant4-v" version
-                    ".tar.gz"))
-              (sha256
-               (base32
-		"1xcj4vigk7mb7wylr5zr3c7ac4ajdcfn786cvag633cxg2kbwpsc"))))))
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "http://cern.ch/geant4-data/releases/geant4-v"
+                           version ".tar.gz"))
+       (sha256
+        (base32 "1xcj4vigk7mb7wylr5zr3c7ac4ajdcfn786cvag633cxg2kbwpsc"))))))
+
+(define-public dawn
+  (package
+    (name "dawn")
+    (version "3.91a")
+    (source
+     (origin
+       (method url-fetch)
+       (uri "https://geant4.kek.jp/~tanaka/src/dawn_3_91a.tgz")
+       (sha256
+        (base32 "1x7mpi77jylsv8mzsqs0ppchbq147azd0b94i2qq2xhis7m5bn41"))))
+    (build-system gnu-build-system)
+    (native-inputs `(("gcc-toolchain" ,gcc-toolchain)
+                     ("make" ,gnu-make)))
+    (propagated-inputs `(("imagemagick" ,imagemagick)
+                         ("tk" ,tk)))
+    (arguments
+     `(#:make-flags (list (string-append "INSTALL_DIR=" %output "/bin"))
+       #:phases (modify-phases %standard-phases
+                  (replace 'configure
+                    (lambda* (#:key outputs #:allow-other-keys)
+                      #t))
+                  (replace 'check
+                    (lambda* (#:key outputs #:allow-other-keys)
+                      #t))
+                  (add-after 'build 'create-bin-directory
+                    (lambda* (#:key outputs #:allow-other-keys)
+                      (mkdir-p (string-append %output "/bin")) #t))
+                  (add-after 'unpack 'change-install-directory
+                    (lambda* (#:key outputs #:allow-other-keys)
+                      (substitute* "Makefile"
+                        (("/usr/local/bin")
+                         (string-append %output "/bin"))) #t)))))
+    (synopsis "Visualize 3D data generated by Geant4 simulation")
+    (description
+     "Fukui Renderer DAWN (Drawer for Academic WritiNgs) is a renderer,
+which reads 3D geometrical data and visualize them. It is a vectorized
+3D PostScript processor with analytical hidden line/surface removal.
+It aims at precise technical drawing of complex geometries.")
+    (home-page "https://geant4.kek.jp/~tanaka/DAWN/About_DAWN.html")
+    (license license:expat)))
